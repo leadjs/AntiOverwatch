377e1b5cdcc37e431a82ceb6dcadd7311835e6f8
Nativ-e <46235562+Nativ-e@users.noreply.github.com>
2020-01-09 22:04:18 -0800

    Removed unnecessary code

--- AntiOW.js
+++ AntiOW.js
@@ -3,26 +3,22 @@ function rageCheck()
 {
     var value = UI.GetValue( "Rage", "GENERAL", "General", "Enabled" );
     var checked = UI.GetValue( "MISC", "JAVASCRIPT", "Script Items", "Disable Rage (Anti-OW)" );
-    if (value=="0") {}
     if (value=="1") {
         if (checked==true){
             UI.SetValue( "Rage", "GENERAL", "General", "Enabled", false );
             Global.PrintChat("You can not play with ragebot enabled!" + n);
             Global.PrintColor( [ 255, 50, 0, 255 ], "ERROR: YOU HAVE ANTI-OVERWATCH (RAGE) ENABLED!" + n);
         }
-        if (checked==false) {}
 
     }
     var legit = UI.GetValue( "Legit", "GENERAL", "General", "Enabled" );
     var checked2 = UI.GetValue( "MISC", "JAVASCRIPT", "Script Items", "Disable Legit (Anti-OW)" );
-    if (legit=="0") {}
     if (legit=="1") {
         if (checked2==true){
             UI.SetValue( "Legit", "GENERAL", "General", "Enabled", false );
             Global.PrintChat("You can not play with legitbot enabled!" + n);
             Global.PrintColor( [ 255, 50, 0, 255 ], "ERROR: YOU HAVE ANTI-OVERWATCH (LEGIT) ENABLED!" + n);
         }
-        if (checked2==false) {}
 
     }
 }
